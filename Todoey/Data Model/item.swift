d7215efbb4966eb5dbfbd4420890c15b0968d2a6
Otso Lehtinen <otso1995@icloud.com>
2019-02-13 17:18:50 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  TitleView.swift
+//  Todoey
+//
+//  Created by Otso Lehtinen on 13/02/2019.
+//  Copyright © 2019 Otso Lehtinen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
